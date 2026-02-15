4365096bf6651cd299bd6e44d60f6d4e9af15979
BMong <83872410+tomo-hara@users.noreply.github.com>
2026-02-16 08:40:26 +0900

    Title: 정수 명령 처리 8, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

--- 260216/정수 명령 처리 8/process-numeric-commands-8.cpp
+++ 260216/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <map>
 
 using namespace std;
 
@@ -8,10 +9,17 @@ int N;
 string command[10000];
 int A[10000];
 
-// const char gp_cmd_table[] = {
-//     "push_front", "push_back", "pop_front", "pop_back",
-//     "size", "empty", "front", "back"
-// };
+enum CommandType {
+    CMD_PUSH_FRONT,
+    CMD_PUSH_BACK,
+    CMD_POP_FRONT,
+    CMD_POP_BACK,
+    CMD_SIZE,
+    CMD_EMPTY,
+    CMD_FRONT,
+    CMD_BACK,
+    //CMD_UNKNOWN // 매핑되지 않은 명령어 처리용
+};
 
 int main() {
     cin >> N;
@@ -25,27 +33,53 @@ int main() {
 
     // Please write your code here.
     list<int> numbers;
-    for (int i = 0; i < N; i++) {
-        if ("push_front" == command[i]) {
-            numbers.push_front(A[i]);
-        } else if ("push_back" == command[i]) {
-            numbers.push_back(A[i]);
-        } else if ("pop_front" == command[i]) {
-            cout << numbers.front() << endl;
-            numbers.pop_front();
-        } else if ("pop_back" == command[i]) {
-            cout << numbers.back() << endl;
-            numbers.pop_back();
-        }
+    map<string, CommandType> cmd_map;
+    CommandType cmd_type;
 
-        else if ("size" == command[i]) {
-            cout << numbers.size() << endl;
-        } else if ("empty" == command[i]) { // cf.boolalpha / noboolalpha
-            cout << numbers.empty() << endl;
-        } else if ("front" == command[i]) {
-            cout << numbers.front() << endl;
-        } else { //if ("back" == command[i])
-            cout << numbers.back() << endl;
+    cmd_map["push_front"] = CMD_PUSH_FRONT;
+    cmd_map["push_back"]  = CMD_PUSH_BACK;
+    cmd_map["pop_front"]  = CMD_POP_FRONT;
+    cmd_map["pop_back"]   = CMD_POP_BACK;
+    cmd_map["size"]       = CMD_SIZE;
+    cmd_map["empty"]      = CMD_EMPTY;
+    cmd_map["front"]      = CMD_FRONT;
+    cmd_map["back"]       = CMD_BACK;
+
+    for (int i = 0; i < N; i++) {
+        // if (cmd_map.find(command[i]) != cmd_map.end()) {
+            cmd_type = cmd_map[command[i]];
+        // }
+        
+        switch (cmd_type) {
+            case CMD_PUSH_FRONT:
+                numbers.push_front(A[i]);
+                break;
+            case CMD_PUSH_BACK:
+                numbers.push_back(A[i]);
+                break;
+            case CMD_POP_FRONT:
+                cout << numbers.front() << endl;
+                numbers.pop_front();
+                break;
+            case CMD_POP_BACK:
+                cout << numbers.back() << endl;
+                numbers.pop_back();
+                break;
+
+            case CMD_SIZE:
+                cout << numbers.size() << endl;
+                break;
+            case CMD_EMPTY:
+                cout << numbers.empty() << endl;
+                break;
+            case CMD_FRONT:
+                cout << numbers.front() << endl;
+                break;
+            case CMD_BACK:
+                cout << numbers.back() << endl;
+                break;
+            default:
+                break;
         }
     }
 
