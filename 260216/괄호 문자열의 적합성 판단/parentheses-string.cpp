04c5f22ae5931979f5f4c19ae33fa137ddba5062
BMong <83872410+tomo-hara@users.noreply.github.com>
2026-02-16 23:38:32 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260216/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string str;
+
+bool isValidParenthesis()
+{
+    stack<char> parenthesesStack;
+    for (auto c : str) {
+        if (c == '(') {
+            parenthesesStack.push(c);
+        } else {
+            if (parenthesesStack.empty()) return false;
+        }
+    }
+    if (!parenthesesStack.empty()) return false;
+    return true;
+}
+
+int main() {
+    cin >> str;
+
+    // Please write your code here.
+    string output = isValidParenthesis() ?
+                    "Yes" : "No";
+    cout << output << endl;
+
+    return 0;
+}
