dc526ab69bf187d6f6043a5cc5c7c7587f688898
BMong <83872410+tomo-hara@users.noreply.github.com>
2026-03-05 00:52:07 +0900

    Title: 두 수의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260305/두 수의 합/sum-of-two-num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int n, k;
+int arr[100000];
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
