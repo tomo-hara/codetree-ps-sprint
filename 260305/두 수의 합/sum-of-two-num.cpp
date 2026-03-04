9d6f14e7077b70d33dbbc101cc5301fb6de8a20d
BMong <83872410+tomo-hara@users.noreply.github.com>
2026-03-05 00:52:46 +0900

    Title: 두 수의 합, Time: 43ms, Memory: 2MB, Status: Passed - Codetree

--- 260305/두 수의 합/sum-of-two-num.cpp
+++ 260305/두 수의 합/sum-of-two-num.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 int n, k;
 int arr[100000];
+
+unordered_map<int, int> freq;
 
 int main() {
     cin >> n >> k;
@@ -12,6 +15,17 @@ int main() {
     }
 
     // Please write your code here.
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int target = k - arr[i];
+        
+        if (freq.find(target) != freq.end()) {
+            count += freq[target];
+        }
+
+        freq[arr[i]]++;
+    }
+    cout << count << endl;
 
     return 0;
 }
