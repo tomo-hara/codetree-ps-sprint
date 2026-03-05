a70ba84b631a49570d8cadc1cb2355137a3ee592
BMong <83872410+tomo-hara@users.noreply.github.com>
2026-03-05 09:09:05 +0900

    Title: 세 수의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260305/세 수의 합/sum-of-three-num.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int n, k;
+int arr[1000];
+unordered_map<int, int> freq;
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Please write your code here.
+    int count = 0;
+    for (int i = 0; i < n - 2; i++) {
+        int first_choice = arr[i];
+        for (int sub_i = i + 1; sub_i < n; sub_i++) {
+            int target = first_choice - arr[sub_i];
+            if (freq.find(target) != freq.end()) {
+                count += freq[target];
+            }
+            freq[arr[sub_i]]++;
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
